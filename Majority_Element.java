b5546b2df97acfb689d57aa7e52a31d17df7e416
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2026-03-23 09:46:55 +0530

    Update Majority_Element.java

--- Majority_Element.java
+++ Majority_Element.java
@@ -31,3 +31,40 @@ public class Main
 		System.out.println("result is"+result);
 	}
 }
+//power of 2
+public static boolean isPowerOfTwo(int n) {
+    return n > 0 && (n & (n - 1)) == 0;
+}
+
+
+//count set bits
+int n = sc.nextInt();
+System.out.println(Integer.bitCount(n));
+
+//hamming dist
+int x = sc.nextInt();
+int y = sc.nextInt();
+System.out.println(Integer.bitCount(x ^ y));
+
+//trailing zeros in fact
+    int count = 0;
+    while (n > 0) {
+        n /= 5;
+        count += n;
+    }
+    return count;
+}
+
+
+//binary palindrome
+public static boolean isBinaryPalindrome(int n) {
+    String s = Integer.toBinaryString(n);
+    int i = 0, j = s.length() - 1;
+
+    while (i < j) {
+        if (s.charAt(i) != s.charAt(j)) return false;
+        i++;
+        j--;
+    }
+    return true;
+}
